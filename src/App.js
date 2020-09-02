d17a3d8f3d668575cef5a2b642df1e22ef9084e0
iquanghung <QuangHung@gmail.com>
2020-09-03 03:13:45 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Baitap1 from "./baitap1";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Baitap1 />
+    </> 
+  );
+}
+
+export default App;
---
+++ src/baitap1/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./header";
+import Carousel from "./carousel";
+import ListSmartphone from "./listSmartphone";
+import ListLaptop from "./listLaptop";
+import Promotion from "./promotion";
+export default function Baitap1(){
+    return <div>
+        <Header />
+        <Carousel />
+        <ListSmartphone />
+        <ListLaptop />
+        <Promotion />
+    </div>
+}
